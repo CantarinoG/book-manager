eefc5fd7bd43534e7706334e07133cc2f12d5b6f
CantarinoG <guilhermecantarino1709@gmail.com>
2023-09-10 14:37:54 -0300

    Add Recyler View Adapter and AllBooks activity

--- app/src/main/java/cantarino/bookmanager/AllBooks.kt
+++ app/src/main/java/cantarino/bookmanager/AllBooks.kt
@@ -2,10 +2,32 @@ package cantarino.bookmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class AllBooks : AppCompatActivity() {
+
+    private lateinit var rvAll: RecyclerView
+    private lateinit var rvAdapter: BookRecyclerViewAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_all_books)
+
+        rvAll = findViewById(R.id.rvAll)
+        rvAdapter = BookRecyclerViewAdapter(this)
+
+        rvAll.adapter = rvAdapter
+        rvAll.layoutManager = GridLayoutManager(this, 2)
+
+        val books = ArrayList<Book>()
+        books.add(Book(1, "Book1", "Author1", 100,
+            "https://marketplace.canva.com/EAFersXpW3g/1/0/1003w/canva-blue-and-white-modern-business-book-cover-cfxNJXYre8I.jpg",
+        "It's a nice book!", "It's a really, really, really, really, really, really, really, really nice book."))
+        books.add(Book(2, "Book2", "Author2", 200,
+            "https://marketplace.canva.com/EAFersXpW3g/1/0/1003w/canva-blue-and-white-modern-business-book-cover-cfxNJXYre8I.jpg",
+            "It's a nice book!", "It's a really, really, really, really, really, really, really, really nice book."))
+
+        rvAdapter.setBooks(books)
     }
 }
---
+++ app/src/main/java/cantarino/bookmanager/Book.kt
@@ -0,0 +1,3 @@
+package cantarino.bookmanager
+
+class Book(val id: Int, val name: String, val author: String, val pages: Int, val imageURL: String, val shortDesc: String, val longDesc: String)
